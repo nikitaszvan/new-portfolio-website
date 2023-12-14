cb788a3ec9f5d33f86aab2d3609dcb2333aef9c5
Nikita Van <nikitaszvan@gmail.com>
2023-12-13 22:13:20 -0500

    useref implemented

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useRef} from "react";
 import useHover from "./useHover.jsx";
 import AboutData from "./AboutData.jsx";
 import ThirdHero from "./ThirdHero.jsx";
 
 const AboutMe = () => {
-  const {hoverStates, handleMouseEnter, handleMouseLeave } = useHover();
 
+  function changeParentHeight(containerRef) {
+    const element = containerRef.current;
+    if (element) {
+      element.style.height = '20vw';
+    }
+  };
+
+  const containerRef = useRef(null);
   const about = AboutData.map((data, index) => (
     <div 
     key={index} 
-    className={`about-${index+1}-container about-container`}
-    onMouseEnter={() => handleMouseEnter(index)}
-    onMouseLeave={() => handleMouseLeave(index)}>
-        <div className={`header-image-${index+1}-container header-image-container`}>
+    className={`about-${index+1}-container about-container`}>
+        <div className={`header-image-${index+1}-container header-image-container`}
+        onMouseOver={() => changeParentHeight(containerRef.current)}
+        ref={containerRef}>
             <h2 className='about-header'>{data.header}</h2>
             <img className='about-image' src={data.src} alt={data.alt} />
         </div>
